287e9b8149a52efc47f69df362bc61fd739d2380
NEPSEWA <156197307+Itzdino99@users.noreply.github.com>
2026-07-07 00:33:49 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -151,9 +151,8 @@ if price == 0:
         f"Join here:\n{link.invite_link}\n\n"
         f"Valid for {mins} minutes."
     )
-    else
     
-    qr_url = f"https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=upi://pay?pa={UPI_ID}%26am={price}%26cu=INR"
+   return qr_url = f"https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=upi://pay?pa={UPI_ID}%26am={price}%26cu=INR"
     
     markup = InlineKeyboardMarkup()
     markup.add(InlineKeyboardButton("✅ I Have Paid", callback_data=f"paid_{ch_id}_{mins}"))
