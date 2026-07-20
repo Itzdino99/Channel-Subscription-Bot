44b777159d44bcfdc28028a36209aa48ba7c6944
NEPSEWA <156197307+Itzdino99@users.noreply.github.com>
2026-07-20 14:53:43 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,8 +14,8 @@ app = Flask('')
 def home(): return "Bot is running and healthy!"
 
 def run_web():
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
 Thread(target=run_web).start()
