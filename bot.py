636af7d1d2b453d9dd5593f9bc5f0aa09794b746
NEPSEWA <156197307+Itzdino99@users.noreply.github.com>
2026-07-21 15:02:00 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -543,23 +543,23 @@ def kick_expired_users():
 # ==========================
 
 if __name__ == "__main__":
-
     keep_alive()
 
     scheduler = BackgroundScheduler()
-
     scheduler.add_job(
         kick_expired_users,
         "interval",
         minutes=1
     )
-
     scheduler.start()
 
     bot.remove_webhook()
 
     try:
-    print("✅ Bot is running...")
-    bot.infinity_polling(timeout=20, long_polling_timeout=10)
-except Exception as e:
-    print(f"Polling error: {e}")
+        print("✅ Bot is running...")
+        bot.infinity_polling(
+            timeout=20,
+            long_polling_timeout=10
+        )
+    except Exception as e:
+        print(f"Polling error: {e}")
